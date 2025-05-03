d7cd1653b2226567ed04310a04224e8ffc02e2d4
Alexandros Paduraru <alexandrubaca25@gmail.com>
2025-05-03 12:56:53 +0300

    Setting up TailwindCSS

---
+++ src/components/ButtonAddTask.tsx
@@ -0,0 +1,13 @@
+import "../css/ButtonAddTask.css"
+
+function Button() {
+  return (
+    <>
+      <div className="add-task-button">
+        <button>Add Task</button>
+      </div>
+    </>
+  );
+}
+
+export default Button
---
+++ src/pages/Home.tsx
@@ -0,0 +1,14 @@
+import Button from "../components/ButtonAddTask"
+import "../css/Home.css"
+function Home() {
+  return (
+    <>
+    <div className="home-page">
+      <h1 className="text-white">TO DO LIST</h1>
+      <Button />
+    </div>
+    </>
+  )
+}
+
+export default Home
